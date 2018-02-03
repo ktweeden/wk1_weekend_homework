828ed5261d24f145c85f66ca4eb591d5d20cda9d
Kate Preston <ktweeden@googlemail.com>
2018-02-03 09:42:07 +0000

    completed extra homework

--- extension_homework/starting_point/friends.rb
+++ extension_homework/starting_point/friends.rb
@@ -5,3 +5,48 @@ end
 def get_fave_tv_show(person)
   return person[:favourites][:tv_show]
 end
+
+def get_if_like_food(person, food)
+  result = false
+  for item in person[:favourites][:things_to_eat]
+    result = true if item == food
+  end
+  return result
+end
+
+def add_new_friend(person, friend)
+  person[:friends].push(friend)
+end
+
+def remove_friend(person, friend)
+  person[:friends].delete(friend)
+end
+
+def total_money(people_array)
+  total = 0
+  for person in people_array
+    total += person[:monies]
+  end
+  return total
+end
+
+def lend_money(lender, lendee, amount)
+  lender[:monies] -= amount
+  lendee[:monies] += amount
+end
+
+def all_favourite_foods(people_array)
+  faves = []
+  for person in people_array
+    faves += person[:favourites][:things_to_eat]
+  end
+  return faves
+end
+
+def billy_no_mates(people_array)
+  no_mates = []
+  for person in people_array
+    no_mates.push(person) if person[:friends].count == 0
+  end
+  return no_mates
+end
--- extension_homework/starting_point/specs/friends_spec.rb
+++ extension_homework/starting_point/specs/friends_spec.rb
@@ -81,31 +81,55 @@ class TestFriends < MiniTest::Test
   # (e.g. function should return true for @person3 when checked for "stew", false for "spinach")
   # (hint: this would maybe be a function that takes in two arguments, one for the person we're checking and one for the food we want to check)
   def test_get_if_like_food
-    result = get_if_like_food
+    result_stew = get_if_like_food(@person3, "stew")
+    result_spinach = get_if_like_food(@person3, "spinach")
+    assert_equal(true, result_stew)
+    assert_equal(false, result_spinach)
   end
 
   # 4. For a given person, add a new name to their list of friends
   # (hint: Add a new string to the friends array, then test for the length of the array, not the return value of your add_friend method. This will probably need two arguments passed)
-
+  def test_add_new_friend
+    add_new_friend(@person1, "John")
+    assert_equal(5, @person1[:friends].count)
+  end
 
   # 5. For a given person, remove a specific name from their list of friends
   # (hint: Same as above, testing for the length of the array should be sufficient)
-
+  def test_remove_friend
+    remove_friend(@person4, "Rick")
+    assert_equal(2, @person4[:friends].count)
+  end
 
   # 6. Find the total of everyone's money
   # (hint: use the @people array, remember how we checked the total number of eggs yesterday?)
-
+  def test_total_money
+    result = total_money(@people)
+    assert_equal(143, result)
+  end
 
   # 7. For two given people, allow the first person to loan a given value of money to the other
   # (hint: our function will probably need 3 arguments passed to it... the lender, the lendee, and the amount for this function)
   # (hint2: You should test if both the lender's and the lendee's money have changed, maybe two assertions?)
-
+  def test_lend_money
+    lend_money(@person5, @person4, 20)
+    assert_equal(80, @person5[:monies])
+    assert_equal(40, @person4[:monies])
+  end
 
   # 8. Find the set of everyone's favourite food joined together
   # (hint: concatenate the favourites/things_to_eat arrays together)
-
-
+  def test_all_favourite_foods
+    faves = ["charcuterie", "soup","bread", "ratatouille", "stew", "spaghetti", "spinach"]
+    result = all_favourite_foods(@people)
+    assert_equal(faves, result)
+  end
   # 9. Find people with no friends
   # (hint: return an array, there might be more people in the future with no friends!)
 
+  def test_billy_no_mates
+    no_mates = []
+    result = billy_no_mates(@people)
+    assert_equal([@person5], result)
+  end
 end
